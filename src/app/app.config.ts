fd885ae53e21b25bc3a7309be0248c9b03d94746
Aman Shukla <amanshukla7876@gmail.com>
2026-02-10 00:39:19 +0800

    Removed hash routing - using normal routing now

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { withHashLocation } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes)
   ]
 };
